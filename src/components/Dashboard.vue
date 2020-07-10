2ed3c8c916ab608e5cace9394d0a4499c8921241
Hyperstorm321 <45072321+Hyperstorm321@users.noreply.github.com>
2020-07-10 11:04:27 -0800

    Update
    New files

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,42 @@
+<template>
+   <v-container>
+       <v-card>
+            {{ first_name }} {{ last_name}} {{email}}
+       </v-card>
+   </v-container>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8000'
+
+export default {
+    name: 'Dashboard',
+    data(){
+        return{
+            first_name: '',
+            last_name: '',
+            email: ''
+        }
+    },
+    mounted(){
+        axios.get('/api/user').then(response =>{
+            this.first_name = response.data.first_name,
+            this.last_name = response.data.last_name,
+            this.email = response.data.email
+        })
+          .catch(error => {
+                    console.log("ERRRR:: ",error.response.data);
+                });
+        
+        
+    },
+    methods:{
+       
+    }
+    
+}
+</script>
---
+++ src/components/Index.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="4" class="mt-10">
+                <span class="lendinn">LendInn</span> <br>
+                <span class="lendinn-desc">
+                    Need an apartment to rent? <br>
+                    or a house to live? <br>
+                    Worry no more, <br>
+                    LendInn is here! <br>
+                </span>
+                <br>
+                <v-btn class="primary mr-5" large v-on:click="RouteTo"> Get Started </v-btn>
+                <v-btn color="primary" outlined large> Search </v-btn>
+            </v-col> 
+
+            <v-col cols="4">
+                <v-img src="../assets/house.jpg"></v-img>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    Name:'Index',
+    methods:{
+        RouteTo(){
+            this.$router.push({ name: 'login' })
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+.lendinn{
+    font-size: 50px;
+    font-weight: bolder;
+    color: teal;
+}
+
+.lendinn-desc{
+    font-size: 40px;
+    
+}
+
+</style>
