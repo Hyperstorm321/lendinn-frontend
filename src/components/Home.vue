2880fed297e56839235c340ab82ca2976eb32c38
Hyperstorm321 <45072321+Hyperstorm321@users.noreply.github.com>
2020-07-16 10:02:48 -0800

    UPDATE
    BACKEND PALA NA PUSH KO HAHAHA

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,15 @@
 <template>
    <v-container>
-       <v-card>
-            {{ first_name }} {{ last_name}} {{email}} {{userid}}
-
-            
-       </v-card>
+               <v-row justify="center">
+                     <v-card v-for="prop in allProperties" :key="prop.property_id" class="ml-5 mb-5">
+                        <img :src="require(`../assets/properties/${prop.main_photo_src}`)" width="300px" height="250px">
+                            <div class="text-center mb-5 mt-5">
+                                {{ prop.name }} <br>
+                                {{ prop.detailed_address }} <br> <br>
+                                <v-btn class="primary">View</v-btn>
+                            </div>
+                    </v-card>
+               </v-row>
    </v-container>
 </template>
 
@@ -19,25 +24,33 @@ export default {
     name: 'Dashboard',
     data(){
         return{
-            userid:'',
-            first_name: '',
-            last_name: '',
-            email: ''
+            userInfo:{
+                userid:''
+            },
+           allProperties:[]
         }
     },
     mounted(){
-        axios.get('api/user').then(response =>{
-            this.first_name = response.data.first_name,
-            this.last_name = response.data.last_name,
-            this.email = response.data.email
-            this.userid = response.data.user_id
-        })
-        .catch(error => {
-            console.log("ERRRR:: ",error.response.data);
-        });
+        this.getUserInfo()
+        this.getAllProperties()
     },
     methods:{
-       
+       getUserInfo(){
+            axios.get('api/user').then(response =>{
+                this.userInfo.userid = response.data.user_id
+            })
+            .catch(error => {
+                console.log("ERRRR:: ",error.response.data);
+            })
+       },
+       getAllProperties(){
+           axios.get('api/usersproperties').then(response =>{
+               this.allProperties = response.data
+           })
+           .catch(error =>{
+               console.log('eroro')
+           })
+       }
     }
     
 }
--- src/components/dashboard_storage/ManageProperties.vue
+++ src/components/dashboard_storage/ManageProperties.vue
@@ -1,117 +1,110 @@
 <template>
     <v-container>
-        <h1>My Properties</h1>
-        <v-row>
-            <v-col cols="12"> 
-                <v-row>
-                    <v-card v-for="(item,i) in items" :key="i" class="ml-5 mb-5">
-                        <img :src="item.src" width="500px"> <br>
-                        <div class="text-center mb-5 mt-5">
-                            <!-- Modal -->
-                                <v-dialog v-model="dialog" width="600" >
-                                    <template v-slot:activator="{ on, attrs }">
-                                        <v-btn color="primary" v-bind="attrs" v-on="on"> Details </v-btn>
-                                    </template>
-                                        <v-card>
-                                            <v-card-title class="headline teal white--text"> Property Details | LendInn </v-card-title>
-                                                <v-card-text>
-                                                    <v-row>
-                                                        <v-col cols="4">
-                                                            <v-text-field label="Name of Agency"></v-text-field>
-                                                        </v-col>
-                                                         <v-col cols="4">
-                                                            <v-text-field label="Property Name"></v-text-field>
-                                                        </v-col>
-                                                        <v-col cols="4">
-                                                            <v-text-field label="Location"></v-text-field>
-                                                        </v-col>
-                                                    </v-row>
+        <h1>My Properties</h1> 
+        <!-- Start of Create dialog -->
+            <v-dialog v-model="dialog" width="600">
+                <template v-slot:activator="{ on,attrs}">
+                    <v-btn color="primary" v-bind="attrs" v-on="on">Create</v-btn>
+                </template>
+                    <v-card>
+                        <v-card-title class="headline teal white--text"> Create an Property | LendInn</v-card-title>
+                        <v-card-text>
+                            <v-row>
+                                <v-col cols="4">
+                                    <v-text-field label="Name of Agency"></v-text-field>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-text-field label="Property Name"></v-text-field>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-text-field label="Location"></v-text-field>
+                                </v-col>
+                            </v-row>
 
-                                                    <v-row>
-                                                        <v-col cols="4">
-                                                            <v-text-field label="Property Type"></v-text-field>
-                                                        </v-col>
-                                                         <v-col cols="4">
-                                                            <v-text-field label="Land Size"></v-text-field>
-                                                        </v-col>
-                                                        <v-col cols="4">
-                                                            <v-text-field label="Living Size"></v-text-field>
-                                                        </v-col>
-                                                    </v-row>
+                            <v-row>
+                                <v-col cols="4">
+                                    <v-text-field label="Property Type"></v-text-field>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-text-field label="Land Size"></v-text-field>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-text-field label="Living Size"></v-text-field>
+                                </v-col>
+                            </v-row>
 
-                                                    <v-row>
-                                                        <v-col cols="4">
-                                                            <v-text-field label="Bedroom"></v-text-field>
-                                                        </v-col>
-                                                        <v-col cols="4">
-                                                            <v-text-field label="Bathroom"></v-text-field>
-                                                        </v-col>
-                                                        <v-col cols="4">
-                                                            <v-text-field label="Sale/Rent"></v-text-field>
-                                                        </v-col>
-                                                    </v-row>
+                            <v-row>
+                                <v-col cols="4">
+                                    <v-text-field label="Bedroom"></v-text-field>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-text-field label="Bathroom"></v-text-field>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-text-field label="Sale/Rent"></v-text-field>
+                                </v-col>
+                            </v-row>
 
-                                                    <v-row>
-                                                        <v-col cols="12">
-                                                            <v-text-field label="Home Features/Amenities"></v-text-field>
-                                                            <v-text-field label="Details"></v-text-field>
-                                                             <v-text-field label="Pet Allowed"></v-text-field>
-                                                        </v-col>
-                                                    </v-row>
-
-                                                </v-card-text>
-                                            <v-card-actions>
-                                            <v-spacer></v-spacer>
-                                            <v-btn color="primary"> I Accept</v-btn>
-                                            </v-card-actions>
-                                        </v-card>
-                                 </v-dialog>
-                            <v-btn class="error ml-5">Delete</v-btn>
-                        </div>
+                            <v-row>
+                                <v-col cols="12">
+                                    <v-text-field label="Home Features/Amenities"></v-text-field>
+                                    <v-text-field label="Details"></v-text-field>
+                                    <v-text-field label="Pet Allowed"></v-text-field>
+                                </v-col>
+                            </v-row>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="primary"> Create </v-btn>
+                        </v-card-actions>
                     </v-card>
-                </v-row>
-            </v-col>
-        </v-row>
+            </v-dialog>
+        <!-- End of Create Dialog --> 
+              <v-row justify="center" class="mt-10">
+                   <v-card v-for="prop in myProperties" :key="prop.property_id" class="ml-5 mb-5">
+                       <img :src="require(`../../assets/properties/${prop.main_photo_src}`)" width="300px" height="250px">
+                       <div class="text-center mt-5 mb-5">
+                           {{prop.name}} <br>
+                           {{prop.detailed_address}}<br> <br>
+                           <v-btn class="primary mr-5">Details</v-btn>
+                           <v-btn class="error">Remove</v-btn>
+                       </div>
+                   </v-card>
+               </v-row>
     </v-container>
 </template>
 
 <script>
 
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8000'
+
 export default {
       Name: 'ManageProperties',
       data(){
           return{
-               items: [
-                {
-                src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-                desc: 'heha'
-                },
-                {
-                src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-                desc: 'hehi'
-                },
-                {
-                src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-                desc: 'hehe'
-                },
-                {
-                src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-                desc: 'heho'
-                },
-                {
-                src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-                desc: 'hehu'
-                },
-                {
-                src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-                desc: 'heho'
-                },
-                
-            ],
+            userId:'',
+            myProperties:[]
           }
       },
+      mounted(){
+          this.getMyProperties()
+      },
       methods:{
-
+          getMyProperties(){
+              let id;
+              axios.get('/api/user').then(response =>{
+                    id = response.data.user_id
+                    axios.get(`api/myproperties/${id}`).then(response =>{
+                        this.myProperties = response.data
+                    })
+              })
+              .catch(error => {
+                  console.log(error)
+              })
+          }
       }
 }
 </script>
